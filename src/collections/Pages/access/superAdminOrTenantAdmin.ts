aeffd718d24f267e0bf8ca9f48146f1ddcd09e71
nomorechokedboy <haiduongit@protonmail.com>
2025-08-23 18:36:36 +0700

    feat: enable pages creation for tenant admin

--- src/collections/Pages/access/superAdminOrTenantAdmin.ts
+++ src/collections/Pages/access/superAdminOrTenantAdmin.ts
@@ -1,25 +1,33 @@
-import { getUserTenantIDs } from '@/utilities/getUserTenantIDs'
-import { isSuperAdmin } from '../../../access/isSuperAdmin'
-import { Access } from 'payload'
+import { getUserTenantIDs } from "@/utilities/getUserTenantIDs";
+import { isSuperAdmin } from "../../../access/isSuperAdmin";
+import { Access } from "payload";
 
 /**
  * Tenant admins and super admins can will be allowed access
  */
 export const superAdminOrTenantAdminAccess: Access = ({ req }) => {
   if (!req.user) {
-    return false
+    return false;
   }
+
+  const { user } = req;
 
   if (isSuperAdmin(req.user)) {
-    return true
+    return true;
   }
 
-  const adminTenantAccessIDs = getUserTenantIDs(req.user, 'tenant-admin')
-  const requestedTenant = req?.data?.tenant
-
-  if (requestedTenant && adminTenantAccessIDs.includes(requestedTenant)) {
-    return true
-  }
-
-  return false
-}
+  /* const adminTenantAccessIDs = getUserTenantIDs(req.user, "tenant-admin");
+      const requestedTenant = req?.data?.tenant;
+      console.log({ adminTenantAccessIDs, requestedTenant });
+    
+      if (requestedTenant && adminTenantAccessIDs.includes(requestedTenant)) {
+        return true;
+      }
+    
+      return false; */
+  return {
+    "tenants.tenant": {
+      in: getUserTenantIDs(user, "tenant-admin"),
+    },
+  };
+};
